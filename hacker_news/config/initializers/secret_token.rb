55e5a9e515fe90f59f6a6eeffec447d5969bc1a1
David Fisher <tibbon@gmail.com>
2013-11-08 09:43:43 -0500

    Added gems

---
+++ hacker_news/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HackerNews::Application.config.secret_key_base = '5d9e8fb29098c605f404cfae334da66fdebf99f0e40a60764a34dacc8f9ff06de13458bd41feb6fa3f2c2183b6fbda8461c71aa677c127ef4e7ff1e26a00c127'
